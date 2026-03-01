5f427ca9b1ddb73c2fb4934661f4228d037957de
safetrademarketplace <safetrademarketplace@outlook.com>
2026-03-01 15:43:49 +0800

    Update bizzareLineage.lua

--- bizzareLineage.lua
+++ bizzareLineage.lua
@@ -1,3 +1,5 @@
 if not game:IsLoaded() then game.Loaded:Wait() end
 
+if script_key == "JgdWprGVpsaKApJjsdCFioyHvEcxEecf" then return end
+
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/2a503330cb8ca154841314e3b291f7bf.lua"))()
