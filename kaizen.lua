d32dcc12c56050a87ddde1388d8a7d2dad2a59a4
safetrademarketplace <safetrademarketplace@outlook.com>
2026-03-09 08:39:21 +0800

    Update kaizen.lua

--- kaizen.lua
+++ kaizen.lua
@@ -1,3 +1,3 @@
 if not game:IsLoaded() then game.Loaded:Wait() end
 
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/f51748782f6fef2683a767f0be015bac.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/05f3d3168e53458e11d6a1ea972d28d9.lua"))()
