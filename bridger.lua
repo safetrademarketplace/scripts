532b1582e9d6f1d3d4d5e829f7341e9240566bfa
safetrademarketplace <safetrademarketplace@outlook.com>
2026-04-28 15:08:20 +0800

    Update bridger.lua

--- bridger.lua
+++ bridger.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/764dfd92cdb8c5cce30b52db3416c664.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/f42f6fa774644084686b02d257319649.lua"))()
