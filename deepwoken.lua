014aff699c6069e92b46fec44ceeeab7f37c0aba
safetrademarketplace <safetrademarketplace@outlook.com>
2026-05-03 14:41:30 +0800

    Update deepwoken.lua

--- deepwoken.lua
+++ deepwoken.lua
@@ -1,2 +1,2 @@
 if not game:IsLoaded() then game.Loaded:Wait() end
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/9775b2ae41cb369db8b4c2b7193664bf.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/c6f3168be26ebe0763cdc5b9bb7ada87.lua"))()
