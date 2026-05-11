5474370bb0372e6ab7d23e52862bf194c9808d36
safetrademarketplace <safetrademarketplace@outlook.com>
2026-05-11 08:46:44 +0800

    Update paradox.lua

--- paradox.lua
+++ paradox.lua
@@ -1,5 +1,5 @@
 if not game:IsLoaded() then game.Loaded:Wait() end
 local url = (game.PlaceId == 9870517705) 
     and "https://raw.githubusercontent.com/whodunitwww/cerberus-helpers/refs/heads/main/paradox/lobby_script.lua" 
-    or "https://api.luarmor.net/files/v4/loaders/eb151426cc40bf2ea1713bc4df3c50c9.lua"
+    or "https://raw.githubusercontent.com/safetrademarketplace/scripts/refs/heads/main/paradox_script.lua"
 loadstring(game:HttpGet(url))()
